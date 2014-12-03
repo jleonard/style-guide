b66d6e279560cc04eb9746d85f5e09f1a1546bc2
jleonard <thejohnleonard@gmail.com>
2014-12-03 14:09:26 -0500

    adding browser logo info

--- compile.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-echo -e "\033[42;30mmoving readme into harp project...\033[0m";
-cp README.md src/index.md
-harp compile src dist;
-mv -v dist/* .;
-#mv -v dist/ ..;
-#rm -r dist;
-#rm -r src/index.md;
